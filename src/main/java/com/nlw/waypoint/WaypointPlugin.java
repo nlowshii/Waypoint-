4c27d0774ff1c875bad68eb7743f2709da00857b
nlowshii <luxxstl14@gmail.com>
2026-06-30 04:28:13 +0700

    Update WaypointPlugin.java

--- src/main/java/com/nlw/waypoint/WaypointPlugin.java
+++ src/main/java/com/nlw/waypoint/WaypointPlugin.java
@@ -13,64 +13,89 @@ import java.util.*;
 public class WaypointPlugin extends JavaPlugin {
 
     private final Map<String, Location> waypoints = new HashMap<>();
-    private final Map<String, TextDisplay> beamLabels = new HashMap<>();
+    private final Map<String, List<TextDisplay>> beamDisplays = new HashMap<>();
     private final Map<UUID, Map<String, TextDisplay>> playerLabels = new HashMap<>();
 
     private static final double HIDE_DISTANCE = 10.0;
     private static final double LABEL_OFFSET = 8.0;
+    private static final int BEAM_HEIGHT = 8;
+    private static final int BEAM_INTERVAL = 4;
 
     @Override
     public void onEnable() {
-        getLogger().info("WaypointPlugin enabled!");
+        getLogger().info("BetterWaypoint enabled!");
         loadWaypoints();
-        startBeamTask();
+        startParticleTask();
         startPlayerLabelTask();
     }
 
     @Override
     public void onDisable() {
-        for (TextDisplay d : beamLabels.values()) if (d != null) d.remove();
+        for (List<TextDisplay> list : beamDisplays.values())
+            for (TextDisplay d : list) if (d != null) d.remove();
         for (Map<String, TextDisplay> map : playerLabels.values())
             for (TextDisplay d : map.values()) if (d != null) d.remove();
         saveWaypoints();
-        getLogger().info("WaypointPlugin disabled!");
+        getLogger().info("BetterWaypoint disabled!");
     }
 
-    private void spawnBeamLabel(String name, Location loc) {
-        Location labelLoc = loc.clone().add(0, 25, 0);
-        TextDisplay display = (TextDisplay) loc.getWorld().spawnEntity(labelLoc, EntityType.TEXT_DISPLAY);
-        display.setText(name);
-        display.setBillboard(display.getBillboard().CENTER);
-        display.setSeeThrough(false);
-        display.setViewRange(64);
-        org.bukkit.util.Transformation t = new org.bukkit.util.Transformation(
-            new org.joml.Vector3f(0, 0, 0),
-            new org.joml.AxisAngle4f(0, 0, 0, 1),
-            new org.joml.Vector3f(5, 5, 5),
-            new org.joml.AxisAngle4f(0, 0, 0, 1)
-        );
-        display.setTransformation(t);
-        beamLabels.put(name, display);
+    private void spawnBeam(String name, Location loc) {
+        List<TextDisplay> list = new ArrayList<>();
+        int maxHeight = loc.getWorld().getEnvironment() == World.Environment.NETHER
+            ? Math.min(BEAM_HEIGHT, 64) : BEAM_HEIGHT;
+
+        for (int y = 0; y <= maxHeight; y += BEAM_INTERVAL) {
+            Location pos = loc.clone().add(0, y, 0);
+            TextDisplay display = (TextDisplay) loc.getWorld().spawnEntity(pos, EntityType.TEXT_DISPLAY);
+            display.setText("§f" + name);
+            display.setBillboard(display.getBillboard().CENTER);
+            display.setSeeThrough(true);
+            display.setViewRange(128);
+            org.bukkit.util.Transformation t = new org.bukkit.util.Transformation(
+                new org.joml.Vector3f(0, 0, 0),
+                new org.joml.AxisAngle4f(0, 0, 0, 1),
+                new org.joml.Vector3f(1.5f, 1.5f, 1.5f),
+                new org.joml.AxisAngle4f(0, 0, 0, 1)
+            );
+            display.setTransformation(t);
+            list.add(display);
+        }
+
+        beamDisplays.put(name, list);
     }
 
-    private void startBeamTask() {
+    private void startParticleTask() {
         new BukkitRunnable() {
             @Override
             public void run() {
                 for (Map.Entry<String, Location> entry : waypoints.entrySet()) {
                     Location loc = entry.getValue();
                     World world = loc.getWorld();
-                    int maxHeight = world.getEnvironment() == World.Environment.NETHER ? 128 : 300;
-                    for (double y = 0; y <= maxHeight; y += 0.5) {
+                    int maxHeight = world.getEnvironment() == World.Environment.NETHER
+                        ? Math.min(BEAM_HEIGHT, 64) : BEAM_HEIGHT;
+
+                    for (int y = 0; y <= maxHeight; y += BEAM_INTERVAL) {
+                        // particle di atas teks
                         world.spawnParticle(
                             Particle.END_ROD,
-                            loc.getX(), loc.getY() + y, loc.getZ(),
-                            1, 0.0, 0.0, 0.0, 0
+                            loc.getX(), loc.getY() + y + 0.8, loc.getZ(),
+                            3, 0.05, 0, 0.05, 0
+                        );
+                        // particle di bawah teks
+                        world.spawnParticle(
+                            Particle.END_ROD,
+                            loc.getX(), loc.getY() + y - 0.8, loc.getZ(),
+                            3, 0.05, 0, 0.05, 0
                         );
                     }
                 }
             }
-        }.runTaskTimer(this, 0L, 3L);
+        }.runTaskTimer(this, 0L, 5L);
+    }
+
+    private void removeBeam(String name) {
+        List<TextDisplay> list = beamDisplays.remove(name);
+        if (list != null) for (TextDisplay d : list) if (d != null) d.remove();
     }
 
     private void startPlayerLabelTask() {
@@ -168,7 +193,7 @@ public class WaypointPlugin extends JavaPlugin {
             double z = config.getDouble(path + ".z");
             Location loc = new Location(world, x, y, z);
             waypoints.put(name, loc);
-            spawnBeamLabel(name, loc);
+            spawnBeam(name, loc);
         }
     }
 
@@ -183,13 +208,12 @@ public class WaypointPlugin extends JavaPlugin {
             case "setwaypoint" -> {
                 Location loc = player.getLocation();
                 waypoints.put(name, loc);
-                spawnBeamLabel(name, loc);
+                spawnBeam(name, loc);
                 saveWaypoints();
             }
             case "delwaypoint" -> {
                 waypoints.remove(name);
-                TextDisplay old = beamLabels.remove(name);
-                if (old != null) old.remove();
+                removeBeam(name);
                 for (Map<String, TextDisplay> map : playerLabels.values()) {
                     TextDisplay d = map.remove(name);
                     if (d != null) d.remove();
